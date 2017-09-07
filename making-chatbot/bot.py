c2c8ce669025c2ffcdec19cb58814690498d8914
soon0698 <31674784+soon0698@users.noreply.github.com>
2017-09-07 20:54:43 +0900

    Add files via upload

---
+++ making-chatbot/bot.py
@@ -0,0 +1,26 @@
+import praw
+import config
+import time
+
+def bot_login(): 
+    r = praw.Reddit(username = config.username,
+     password = config.password,
+     client_id = config.client_id,
+     client_secret = config.client_secret,
+     user_agent = "soon0698's dog comment responder v0.1")
+    print("로그인(정보 획득) 완료!")
+    return r
+
+def run_bot(r):
+    print("25개의 코멘트를 검색합니다.")
+    for comment in r.subreddit('test').comments(limit=25):
+        if "dog" in comment.body:
+            print("해당 검색어를 찾았습니다. 코멘트 아이디 : " + comment.id)
+            comment.reply("나도 좋아해! [여기](http://imgur.com/gallery/TUQvrsV)에도 하나 있지!")
+            print(comment.id + "의 코멘트에 응답했습니다!")
+    print("10초간 대기합니다.")
+    time.sleep(10)
+
+r = bot_login()
+while True:
+    run_bot(r)
